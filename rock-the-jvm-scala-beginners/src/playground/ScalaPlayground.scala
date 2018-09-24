fca68412c3da4846c513bc5a937e2294e2fa58fe
SergiOn <onishhenko@gmail.com>
2018-09-24 22:32:50 +0300

    #rtjvm-scala-and-functional-programming-for-beginners # section 1, lecture 1 # Getting Started!

---
+++ rock-the-jvm-scala-beginners/src/playground/ScalaPlayground.scala
@@ -0,0 +1,15 @@
+package playground
+
+//object ScalaPlayground extends App {
+//
+//  println("Hello, Scala")
+//
+//}
+
+object ScalaPlayground {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, Scala")
+  }
+
+}
