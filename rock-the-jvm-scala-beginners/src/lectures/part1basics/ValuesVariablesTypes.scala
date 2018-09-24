565d8b598172ca55a2a219b3bd864dd85db44c4e
SergiOn <onishhenko@gmail.com>
2018-09-24 22:52:56 +0300

    #rtjvm-scala-and-functional-programming-for-beginners # section 2, lecture 2 # Values, Variables and Types

---
+++ rock-the-jvm-scala-beginners/src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,38 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  val x: Int = 42
+//  val x = 42
+  println(x)
+
+//  val aString: String = "hello"; val anotherString = "goodbye"
+
+  val aString: String = "hello"
+  val anotherString = "goodbye"
+
+  val aBoolean: Boolean = false
+
+  val aChar: Char = 'a'
+
+  val anInt: Int = x
+
+  val aShort: Short = 4613
+
+  val aLong: Long = 56776766554545L
+
+  val aFloat: Float = 2.0f
+
+  val aDouble: Double = 3.14
+
+  // variables
+
+  var aVariable: Int = 4
+
+  aVariable = 5 // side effects
+
+
+
+
+
+}
