56d1decbfd33b61b35049166397b596d85104b9f
SergiOn <onishhenko@gmail.com>
2018-09-30 00:24:47 +0300

    #rtjvm-scala-and-functional-programming-for-beginners # section 3, lecture 12 # Syntactic Sugar: Method Notations

---
+++ rock-the-jvm-scala-beginners/src/lectures/part2oop/MethodNotations.scala
@@ -0,0 +1,50 @@
+package lectures.part2oop
+
+object MethodNotations extends App {
+
+  class Person(val name: String, favoriteMovie: String) {
+    def likes(movie: String): Boolean = movie == favoriteMovie
+    def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def unary_! : String = s"$name, what the heck?!"
+    def isAlive: Boolean = true
+    def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+  }
+
+  val mary = new Person("Mary", "Inception")
+  println(mary.likes("Inception"))
+  println(mary likes "Inception") // equivalent
+  // infix notation = operator notation (syntactics sugar)
+
+  // "operators" in scala
+
+  val tom = new Person("Tom", "Fight Club")
+  println(mary hangOutWith tom)
+  println(mary + tom)
+  println(mary.+(tom))
+
+  println(1 + 2)
+  println(1.+(2))
+
+  // ALL OPERATORS ARE METHODS
+  // Akka actors have ! ?
+
+  // prefix notation
+
+  val x = -1
+  val y = 1.unary_-
+
+  println(!mary)
+  println(mary.unary_!)
+
+  // postfix notation
+
+  println(mary.isAlive)
+  println(mary isAlive)
+
+  // apply
+
+  println(mary.apply())
+  println(mary()) // equivalent
+
+}
