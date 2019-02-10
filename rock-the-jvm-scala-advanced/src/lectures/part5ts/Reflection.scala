dc6cbe0e33ee382628b04a232acc2228925462c6
SergiOn <onishhenko@gmail.com>
2019-02-10 17:13:17 +0200

    #rtjvm-advanced-scala-and-functional-programming # section 6, lecture 53 # Reflection, Part 2

--- rock-the-jvm-scala-advanced/src/lectures/part5ts/Reflection.scala
+++ rock-the-jvm-scala-advanced/src/lectures/part5ts/Reflection.scala
@@ -52,4 +52,71 @@ object Reflection extends App {
   method.apply()
 
 
+  // type erasure
+
+  // pp #1: differentiate types at runtime
+  val numbers = List(1,2,3)
+  numbers match  {
+    case listOfStrings: List[String] => println("list of strings")
+    case listOfNumbers: List[Int] => println("list of numbers")
+
+    // because of compile process
+//    case listOfStrings: List => println("list of strings")
+//    case listOfNumbers: List => println("list of numbers")
+  }
+
+  // pp #2: limitations on overloads
+//  def processList(list: List[Int]): Int = 43
+//  def processList(list: List[String]): Int = 45
+
+  // because of time compilation
+//  def processList(list: List): Int = 43
+//  def processList(list: List): Int = 45
+
+
+  // TypeTags
+
+  // 0 - import
+  import ru._
+
+  // 1 - creating a type tag "manually"
+  val ttag = typeTag[Person]
+  println(ttag.tpe)
+
+  class MyMap[K, V]
+
+  // 2 - pass type tags as implicit parameters
+  def getTypeArguments[T](value: T)(implicit typeTag: TypeTag[T]) = typeTag.tpe match {
+    case TypeRef(_, _, typeArguments) => typeArguments
+    case _ => List()
+  }
+
+  val myMap = new MyMap[Int, String]
+  val typeArgs = getTypeArguments(myMap) // (typeTag: TypeTag[MyMap[Int,String]])
+  println(typeArgs) // typeArguments -> List(Int, String)
+
+  def isSubtype[A, B](implicit ttagA: TypeTag[A], ttagB: TypeTag[B]): Boolean = {
+    ttagA.tpe <:< ttagB.tpe
+  }
+
+  class Animal
+  class Dog extends Animal
+  println(isSubtype[Dog, Animal]) // true
+
+
+  // I have an instance
+  // method name computed from somewhere else
+  // 1 - mirror
+  // 2 - reflect the instance
+
+  // 3 - method symbol
+//  val methodSymbol = ru.typeOf[Person].decl(ru.TermName(methodName)).asMethod
+  val anotherMethodSymbol = typeTag[Person].tpe.decl(ru.TermName(methodName)).asMethod
+
+  // 4 - reflect the method = can DO things
+  val sameMethod = reflected.reflectMethod(anotherMethodSymbol)
+
+  // 5 - invoke the method
+  sameMethod.apply()
+
 }
